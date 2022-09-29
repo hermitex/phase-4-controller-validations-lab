864865847bf01a0cb4299ea6d1d4dd1cfceba259
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-09-29 12:05:31 +0300

    commit tree 1aaadcb41013cd2d72ccf43ef81bc5aae25fc38a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates(:content, {:length => {:minimum => 100}})
+  validate :category_must_be_fiction_or_non_fiction
+
+  private
+  def category_must_be_fiction_or_non_fiction
+    unless category == "Fiction" || category == "Non-Fiction"
+      errors.add(:category, "is not included in the list")
+    end
+  end
 end
